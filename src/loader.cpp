dc87f829025b838a36fc891a882bdd7dff851f74
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-15 09:40:46 +0200

    loader: make console messages more user-friendly

--- src/loader.cpp
+++ src/loader.cpp
@@ -6,7 +6,7 @@ typedef void (*PlayFunction)(char const* url);
 
 int main(int argc, char* argv[]) {
 	if(argc != 3) {
-		fprintf(stderr, "Usage: %s <my_library>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <my_library> <my_url>\n", argv[0]);
 		return 1;
 	}
 
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
 
 	auto handle = dlopen(lib, RTLD_NOW);
 	if(!handle) {
-		fprintf(stderr, "can't load '%s': \n", argv[1]);
+		fprintf(stderr, "can't load '%s' (%s)\n", argv[1], dlerror());
 		return 1;
 	}
 
