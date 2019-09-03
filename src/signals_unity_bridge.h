ea2fca3755d9bfc7953aaf05f3be4dcee4f88d82
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-09-03 23:01:09 +0200

    API doc + update signals (use publish time to avoid failing clocks)

--- src/signals_unity_bridge.h
+++ src/signals_unity_bridge.h
@@ -52,6 +52,7 @@ SUB_EXPORT bool sub_play(sub_handle* h, const char* URL);
 // Returns: the size of compressed data actually copied,
 // or zero, if no frame was available for this stream.
 // If 'dst' is null, the frame will not be dequeued, but its size will be returned.
+// Note that you shall dequeue all data from all streams to avoid being locked.
 SUB_EXPORT size_t sub_grab_frame(sub_handle* h, int streamIndex, uint8_t* dst, size_t dstLen, FrameInfo* info);
 }
 
